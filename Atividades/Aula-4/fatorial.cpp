ccce93925837be3ca61fc5711d61676448b4bda8
Carlos Fernando dos Santos <fer.1972@hotmail.com>
2026-03-30 20:04:55 -0300

    - Melhorando o calculo de fatorial

--- Atividades/Aula-4/fatorial.cpp
+++ Atividades/Aula-4/fatorial.cpp
@@ -1,20 +1,20 @@
-#include <stdio.h>
 #include <iostream>
-
 using namespace std;
 
 int fatorial(int n){
-    if(n == 0){
+    if(n == 0)
         return 1;
-    } else {
+    else
         return n * fatorial(n-1);
-    }
 }
 
 int main(){
-    int res;
-    res = fatorial(5);
-    cout << "O Fatorial: " << res << endl;
+    int numero;
+
+    cout << "Digite um numero: ";
+    cin >> numero;
+
+    cout << "O Fatorial de " << numero << " eh: " << fatorial(numero) << endl;
 
     return 0;
 }
