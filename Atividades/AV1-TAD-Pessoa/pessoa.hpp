188ce1b0a7a6b0f269fc3f153b2e05f252d1efbe
Carlos Fernando dos Santos <fer.1972@hotmail.com>
2026-04-01 10:06:34 -0300

    - Implementação do pessoa.hpp

---
+++ Atividades/AV1-TAD-Pessoa/pessoa.hpp
@@ -0,0 +1,19 @@
+#include <string>
+using namespace std;
+
+struct Pessoa 
+{
+    string nome;
+    int idade;
+    string endereco;
+};
+
+typedef struct Pessoa Pessoa;
+
+Pessoa* _pessoa_cria(string nome, int idade, string endereco);
+
+void _pessoa_acessa(Pessoa* p, string* nome, int* idade, string* endereco);
+
+void _pessoa_atribui(Pessoa* p, string nome, int idade, string endereco);
+
+void _pessoa_libera(Pessoa* p);
