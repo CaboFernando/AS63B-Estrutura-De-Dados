61f70ee9975a779dafdf6abab1c1546dc01144cd
Carlos Fernando dos Santos <fer.1972@hotmail.com>
2026-04-01 10:26:29 -0300

    - Implementando métodos para entrada de dados no CRUD

--- Atividades/AV1-TAD-Pessoa/main.cpp
+++ Atividades/AV1-TAD-Pessoa/main.cpp
@@ -2,6 +2,53 @@
 #include "pessoa.cpp"
 using namespace std;
 
+void criarPessoa()
+{
+    string nome, endereco;
+    int idade;
+
+    cout << "Digite o nome da pessoa: ";
+    cin >> nome;
+    cout << "Digite a idade da pessoa: ";
+    cin >> idade;
+    cout << "Digite o endereco da pessoa: ";
+    cin >> endereco;
+
+    Pessoa* p = _pessoa_cria(nome, idade, endereco);
+}
+
+void visualizarPessoa(Pessoa* p)
+{
+    string nome, endereco;
+    int idade;
+
+    _pessoa_acessa(p, &nome, &idade, &endereco);
+
+    cout << "Nome: " << nome << endl;
+    cout << "Idade: " << idade << endl;
+    cout << "Endereco: " << endereco << endl;
+}
+
+void alterarPessoa(Pessoa* p)
+{
+    string nome, endereco;
+    int idade;
+
+    cout << "Digite o novo nome da pessoa: ";
+    cin >> nome;
+    cout << "Digite a nova idade da pessoa: ";
+    cin >> idade;
+    cout << "Digite o novo endereco da pessoa: ";
+    cin >> endereco;
+
+    _pessoa_atribui(p, nome, idade, endereco);
+}
+
+void removerPessoa(Pessoa* p)
+{
+    _pessoa_libera(p);
+}
+
 void menu()
 {
     int opcao;
@@ -13,8 +60,9 @@ void menu()
         cout << "3. Alterar Pessoa" << endl;
         cout << "4. Remover Pessoa" << endl;
         cout << "0. Sair" << endl;
-        cout << "==========================" << endl;
+        cout << "Escolha uma opcao: ";        
         cin >> opcao;
+
     } while(opcao != 0);
 
 }
@@ -23,5 +71,6 @@ int main()
 {
     menu();
 
+    cout << "==========================" << endl;
     return 0;
 }
