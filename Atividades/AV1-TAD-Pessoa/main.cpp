6a1f4fc3f7600953472035bdda4603417fe55bb3
Carlos Fernando dos Santos <fer.1972@hotmail.com>
2026-04-01 10:18:20 -0300

    - Implementando método menu

--- Atividades/AV1-TAD-Pessoa/main.cpp
+++ Atividades/AV1-TAD-Pessoa/main.cpp
@@ -2,8 +2,26 @@
 #include "pessoa.cpp"
 using namespace std;
 
+void menu()
+{
+    int opcao;
+
+    do{
+        cout << "========== MENU ==========" << endl;
+        cout << "1. Criar Pessoa" << endl;
+        cout << "2. Visualizar Pessoa" << endl;
+        cout << "3. Alterar Pessoa" << endl;
+        cout << "4. Remover Pessoa" << endl;
+        cout << "0. Sair" << endl;
+        cout << "==========================" << endl;
+        cin >> opcao;
+    } while(opcao != 0);
+
+}
+
 int main()
 {
+    menu();
 
     return 0;
 }
