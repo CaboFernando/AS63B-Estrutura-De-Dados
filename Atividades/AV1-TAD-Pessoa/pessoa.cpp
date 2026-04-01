e9210d6e2890b60cb96f1c918b25b5438b8dfc78
Carlos Fernando dos Santos <fer.1972@hotmail.com>
2026-04-01 10:10:01 -0300

    - Implementação do método criar

--- Atividades/AV1-TAD-Pessoa/pessoa.cpp
+++ Atividades/AV1-TAD-Pessoa/pessoa.cpp
@@ -2,8 +2,20 @@
 #include "pessoa.hpp"
 using namespace std;
 
-int main()
+Pessoa *_pessoa_cria(string nome, int idade, string endereco)
 {
+    Pessoa *p = new Pessoa; // Aloca memória para um novo Pessoa usando new
 
-    return 0;
+    if(p == nullptr) { // Verifica se a alocação de memória foi bem-sucedida
+        cout << "Erro ao alocar memória para Pessoa" << endl;
+        return nullptr;
+    }
+
+    // Atribui os valores aos campos da estrutura
+    p->nome = nome;
+    p->idade = idade;
+    p->endereco = endereco;
+
+    // Retorna o ponteiro para a estrutura criada
+    return p;
 }
