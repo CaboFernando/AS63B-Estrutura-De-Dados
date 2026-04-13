764ad9f1714aa7faaac87ff448965666620d8117
Carlos Fernando dos Santos <fer.1972@hotmail.com>
2026-04-13 15:20:01 -0300

    - Criando função na struct que atualiza somento o aumento de salário e a atualiza a idade diretamente na struct

--- Atividades/Lista de ATV/Questao-36/funcionario.hpp
+++ Atividades/Lista de ATV/Questao-36/funcionario.hpp
@@ -10,5 +10,11 @@ struct Funcionario
 
 typedef struct Funcionario Funcionario;;
 
+void atualizarDados(Funcionario *f, float aumentoSalario, int idade)
+{
+    if(f == nullptr) return; // Verifica se o ponteiro é nulo antes de acessar os campos
 
+    f->salario += aumentoSalario; // Atualiza o salário do funcionário somando o valor passado como parâmetro
+    f->idade = idade;     // Atualiza a idade do funcionário
+}
 
